3cf160ec78721e318892609c3197f88163c63219
Becki Srofe <blf_23@yahoo.com>
2017-12-20 15:10:51 -0700

    Add all form fields

--- src/components/NewMemberForm.js
+++ src/components/NewMemberForm.js
@@ -5,8 +5,23 @@ class NewMemberForm extends Component {
   constructor(){
     super()
     this.state = {
-      username: '',
+      firstName: '',
+      lastName: '',
+      crossStreet1: '',
+      crossStreet2: '',
+      email: '',
+      phoneNumber: '',
       password: '',
+      street: '',
+      city: '',
+      state: '',
+      zip: '',
+      residenceType: '',
+      fencedYard: '',
+      childrenUnderTwo: '',
+      otherChildren: '',
+      cats: '',
+      profile: ''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -16,7 +31,7 @@ class NewMemberForm extends Component {
   }
   handleSubmit(event) {
     event.preventDefault()
-    const { username, password } = this.state //change name of this varible to newMemberInfo 
+    const { username, password } = this.state //change name of this varible to newMemberInfo
     //axios.post('/new-user', { username, password }).then(response => response)use fetch here
   }
 
@@ -24,10 +39,40 @@ class NewMemberForm extends Component {
     return(
       <section className="content-panel">
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Username: </label>
-            <input name="username" type="text" onChange={this.handleChange}/>
+          <label htmlFor="firstName">First Name: </label>
+            <input name="firstName" type="text" onChange={this.handleChange}/>
+          <label htmlFor="lastName">Last Name: </label>
+            <input name="lastName" type="text" onChange={this.handleChange}/>
+          <label htmlFor="street">Street Address: </label>
+            <input name="street" type="text" onChange={this.handleChange}/>
+          <label htmlFor="city">City: </label>
+            <input name="city" type="text" onChange={this.handleChange}/>
+          <label htmlFor="state">State: </label>
+            <input name="state" type="text" onChange={this.handleChange}/>
+          <label htmlFor="zip">Zip: </label>
+            <input name="zip" type="text" onChange={this.handleChange}/>
+          <label htmlFor="crossStreet1">First Major Cross Street: </label>
+            <input name="crossStreet1" type="text" onChange={this.handleChange}/>
+          <label htmlFor="crossStreet2">Second Major Cross Street: </label>
+            <input name="crossStreet2" type="text" onChange={this.handleChange}/>
+          <label htmlFor="email">Email: </label>
+            <input name="email" type="text" onChange={this.handleChange}/>
+          <label htmlFor="phoneNumber">Phone: </label>
+            <input name="phoneNumber" type="text" onChange={this.handleChange}/>
+          <label htmlFor="residenceType">Residence Type: </label>
+            <input name="residenceType" type="text" onChange={this.handleChange}/>
+          <label htmlFor="fencedYard">Fenced Yard? </label>
+            <input name="fencedYard" type="text" onChange={this.handleChange}/>
+          <label htmlFor="childrenUnderTwo">Children Under The Age Of 2: </label>
+            <input name="childrenUnderTwo" type="text" onChange={this.handleChange}/>
+          <label htmlFor="otherChildren">Children Between 2-12: </label>
+            <input name="otherChildren" type="text" onChange={this.handleChange}/>
+          <label htmlFor="cats">Do You Have Cats?</label>
+            <input name="cats" type="text" onChange={this.handleChange}/>
           <label htmlFor="password">Password: </label>
             <input name="password" type="password" onChange={this.handleChange}/>
+          <label htmlFor="profile">Profile: </label>
+            <input name="profile" type="text" onChange={this.handleChange}/>
           <button type="submit">Submit</button>
         </form>
       </section>
