6232ee510352fe6b5558859236b747ace707fd5e
Becki Srofe <blf_23@yahoo.com>
2017-12-20 14:36:25 -0700

    Add skeleton of new member form

--- src/components/NewMemberForm.js
+++ src/components/NewMemberForm.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react'
 
 
 class NewMemberForm extends Component {
+  constructor(){
+    super()
+    this.state = {
+      username: '',
+      password: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    const { username, password } = this.state //change name of this varible to newMemberInfo 
+    //axios.post('/new-user', { username, password }).then(response => response)use fetch here
+  }
+
   render() {
     return(
       <section className="content-panel">
-        <p>This is the form</p>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username: </label>
+            <input name="username" type="text" onChange={this.handleChange}/>
+          <label htmlFor="password">Password: </label>
+            <input name="password" type="password" onChange={this.handleChange}/>
+          <button type="submit">Submit</button>
+        </form>
       </section>
     )
   }
