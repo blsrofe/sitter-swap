96358de99ceff10c94bfb7b99855e7e60db55f11
Becki Srofe <blf_23@yahoo.com>
2017-12-28 15:32:25 -0700

    Add working link to user dashboard from user profile

--- src/components/ContentPanel.js
+++ src/components/ContentPanel.js
@@ -4,6 +4,7 @@ import Home from './Home'
 import NewMemberBox from './NewMemberBox'
 import UserProfileBoxes from './UserProfileBoxes'
 import NewDogsForm from './NewDogsForm'
+import Dashboard from './Dashboard'
 
 
 class ContentPanel extends Component {
@@ -14,6 +15,7 @@ class ContentPanel extends Component {
           <Route path="/new" component={NewMemberBox}/>
           <Route path="/users/:id/profile" component={UserProfileBoxes}/>
           <Route path="/users/:id/new-dog" component={NewDogsForm}/>
+          <Route path="/users/:id/dashboard" component={Dashboard}/>
         </div>
     )
   }
--- src/components/UserProfileBoxes.js
+++ src/components/UserProfileBoxes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import UserProfile from './UserProfile'
 import DogInfoBox from './DogInfoBox'
+import {NavLink} from "react-router-dom"
 
 class UserProfileBoxes extends Component {
   render() {
@@ -8,6 +9,7 @@ class UserProfileBoxes extends Component {
       <section className="user-profile-box">
         <UserProfile id={this.props.match.params.id}/>
         <DogInfoBox id={this.props.match.params.id}/>
+        <h1><NavLink to={'/users/' + this.props.match.params.id + '/dashboard' }>Visit Your Dashboard</NavLink></h1>
       </section>
     )
   }
