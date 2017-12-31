aaae8190ccee33d3b1d181184b7023968561406b
Becki Srofe <blf_23@yahoo.com>
2017-12-31 12:40:27 -0700

    Fix path

--- src/components/ContentPanel.js
+++ src/components/ContentPanel.js
@@ -14,7 +14,7 @@ class ContentPanel extends Component {
   render() {
     return(
         <div className="content-panel">
-          <Route exact path="/" component={Home}/>
+          <Route path="/" component={Home}/>
           <Route path="/new" component={NewMemberBox}/>
           <Route path="/users/:id/profile" component={UserProfileBoxes}/>
           <Route path="/users/:id/new-dog" component={NewDogsForm}/>
