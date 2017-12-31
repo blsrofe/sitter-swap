b0705f0c06c8496d0f9c676748efa415d6dabac5
Becki Srofe <blf_23@yahoo.com>
2017-12-31 12:00:55 -0700

    Add callback url

--- src/objects/AuthService.js
+++ src/objects/AuthService.js
@@ -1,19 +1,19 @@
-import decode from 'jwt-decode';
-import history from './objects/history'
-import auth0 from 'auth0-js';
-const ID_TOKEN_KEY = 'id_token';
-const ACCESS_TOKEN_KEY = 'access_token';
+import decode from 'jwt-decode'
+import history from './history'
+import auth0 from 'auth0-js'
+const ID_TOKEN_KEY = 'id_token'
+const ACCESS_TOKEN_KEY = 'access_token'
 
-const CLIENT_ID = 'JcoZYiKVAdbW0d6P7QpG1zKnYEYcW1i0';
-const CLIENT_DOMAIN = 'AUTH0_DOMAIN';
-const REDIRECT = 'https://sitter-swap-api.herokuapp.com/callback';
-const SCOPE = 'YOUR_SCOPE';
-const AUDIENCE = 'AUDIENCE_ATTRIBUTE';
+const CLIENT_ID = 'JcoZYiKVAdbW0d6P7QpG1zKnYEYcW1i0'
+const CLIENT_DOMAIN = 'sitter-swap.auth0.com/'
+const REDIRECT = 'https://sitterswap.herokuapp.com/callback'
+const SCOPE = 'YOUR_SCOPE'
+const AUDIENCE = 'https://sitter-swap.com'
 
 var auth = new auth0.WebAuth({
   clientID: CLIENT_ID,
   domain: CLIENT_DOMAIN
-});
+})
 
 export function login() {
   auth.authorize({
@@ -21,18 +21,18 @@ export function login() {
     redirectUri: REDIRECT,
     audience: AUDIENCE,
     scope: SCOPE
-  });
+  })
 }
 
 export function logout() {
-  clearIdToken();
-  clearAccessToken();
-  browserHistory.push('/');
+  clearIdToken()
+  clearAccessToken()
+  history.push('/')
 }
 
 export function requireAuth(nextState, replace) {
   if (!isLoggedIn()) {
-    replace({pathname: '/'});
+    replace({pathname: '/'})
   }
 }
 
@@ -49,43 +49,43 @@ function clearIdToken() {
 }
 
 function clearAccessToken() {
-  localStorage.removeItem(ACCESS_TOKEN_KEY);
+  localStorage.removeItem(ACCESS_TOKEN_KEY)
 }
 
 // Helper function that will allow us to extract the access_token and id_token
 function getParameterByName(name) {
-  let match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
-  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  let match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash)
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
 }
 
 // Get and store access_token in local storage
 export function setAccessToken() {
-  let accessToken = getParameterByName('access_token');
-  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+  let accessToken = getParameterByName('access_token')
+  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken)
 }
 
 // Get and store id_token in local storage
 export function setIdToken() {
-  let idToken = getParameterByName('id_token');
-  localStorage.setItem(ID_TOKEN_KEY, idToken);
+  let idToken = getParameterByName('id_token')
+  localStorage.setItem(ID_TOKEN_KEY, idToken)
 }
 
 export function isLoggedIn() {
-  const idToken = getIdToken();
+  const idToken = getIdToken()
   return !!idToken && !isTokenExpired(idToken);
 }
 
 function getTokenExpirationDate(encodedToken) {
-  const token = decode(encodedToken);
-  if (!token.exp) { return null; }
+  const token = decode(encodedToken)
+  if (!token.exp) { return null;}
 
-  const date = new Date(0);
-  date.setUTCSeconds(token.exp);
+  const date = new Date(0)
+  date.setUTCSeconds(token.exp)
 
-  return date;
+  return date
 }
 
 function isTokenExpired(token) {
   const expirationDate = getTokenExpirationDate(token);
-  return expirationDate < new Date();
+  return expirationDate < new Date()
 }
